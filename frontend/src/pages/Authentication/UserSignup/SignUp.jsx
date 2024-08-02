239403db9adaf73e6a733bd1c4a9b2ef7a3de83b
anoopvr33 <anoopvranoop33@gmail.com>
2024-08-02 11:18:06 +0530

    verify

--- frontend/src/pages/Authentication/UserSignup/SignUp.jsx
+++ frontend/src/pages/Authentication/UserSignup/SignUp.jsx
@@ -4,6 +4,8 @@ import axios from "axios";
 import "./usersignup.css";
 
 const SignUp = () => {
+  const [showOtp, setOtp] = useState(false);
+  const [showReg, setReg] = useState(false);
   const navigate = useNavigate();
   // const [res, setRes] = useState();
   const [cred, setCred] = useState({
@@ -30,6 +32,12 @@ const SignUp = () => {
     navigate(`/registration/${response.data.detail._id}`);
     // }
   };
+  const VButton = () => {
+    setOtp(true);
+  };
+  const Regist12 = () => {
+    setReg(true);
+  };
   // useEffect(() => {
   //   OnReg();
   // }, []);
@@ -54,14 +62,38 @@ const SignUp = () => {
             name="confirmpassword"
             onChange={onRegchange}
           />
+          <br /> <label htmlFor="">Mobile</label>
+          <div className="verify">
+            <input type="number" />
+            <button className="v" onClick={VButton}>
+              Verify
+            </button>
+          </div>
+          {showOtp ? (
+            <div className="otp">
+              <label htmlFor=""></label>
+              <input placeholder="Enter OTP" type="number" name="otp" />
+              <button onClick={Regist12} className="v1">
+                Submit
+              </button>
+            </div>
+          ) : (
+            false
+          )}
           <br />
         </div>
-        <button className="up" onClick={OnReg}>
-          Register
-        </button>
-        {/* <Link to="/registration">signup</Link> */}
+        {showReg ? (
+          <div className="reg12">
+            <button className="up" onClick={OnReg}>
+              Register
+            </button>
+            {/* <Link to="/registration">signup</Link> */}
 
-        <button className="up2">Social media login</button>
+            <button className="up2">Social media login</button>
+          </div>
+        ) : (
+          false
+        )}
       </div>
     </div>
   );

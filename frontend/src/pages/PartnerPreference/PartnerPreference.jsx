9e139334080c86b9ba40f04ee737fabf4a9648a6
anushmashaiju <anushma2015@gmail.com>
2024-09-20 15:48:01 +0530

    anushma15 changed preference selection

--- frontend/src/pages/PartnerPreference/PartnerPreference.jsx
+++ frontend/src/pages/PartnerPreference/PartnerPreference.jsx
@@ -19,9 +19,9 @@ const PartnerPreference = () => {
   const { matrimonyProfileId } = useContext(IdContext);
   const [profilePic, setProfilePic] = useState('');
   const [selectedGender, setSelectedGender] = useState('');
-  const [selectedEducation, setSelectedEducation] = useState('');
-  const [selectedReligion, setSelectedReligion] = useState('');
-  const [selectedOccupation, setSelectedOccupation] = useState('');
+  const [selectedEducations, setSelectedEducations] = useState([]);
+  const [selectedReligions, setSelectedReligions] = useState([]);
+  const [selectedOccupations, setSelectedOccupations] = useState([]);
 
   const navigate = useNavigate(); // Initialize useNavigate
 
@@ -30,26 +30,17 @@ const PartnerPreference = () => {
     setShowProfileOptions(!showProfileOptions);
   };
 
-
-  const removeTag = (type, tag) => {
-    if (type === 'location') {
-      setSelectedLocations(selectedLocations.filter((location) => location !== tag));
-    } else if (type === 'hobby') {
-      setHobbies(hobbies.filter((hobby) => hobby !== tag));
-    }
-  };
-
   const [selectedLocations, setSelectedLocations] = useState([]);
   const keralaDistricts = [
-     'Alappuzha', 'Ernakulam', 'Idukki', 'Kannur', 'Kasargod',
+    'Alappuzha', 'Ernakulam', 'Idukki', 'Kannur', 'Kasargod',
     'Kollam', 'Kottayam', 'Kozhikode', 'Malappuram', 'Palakkad',
     'Pathanamthitta', 'Thiruvananthapuram', 'Thrissur', 'Wayanad'
   ];
-  const interestOptions = [ 'music', 'dance', 'riding', 'travelling', 'sports', 'fitness']
-  const genderOptions = [ 'Male', 'Female', 'Other'];
+  const interestOptions = ['music', 'singing','dance', 'riding', 'travelling', 'sports', 'fitness'];
+  const genderOptions = ['Male', 'Female', 'Other'];
   const educationOptions = ['High School', 'Bachelors', 'Masters', 'Doctorate'];
   const religionOptions = ['Christian', 'Islam', 'Hindu', 'Other'];
-  const occupationOptions = ['IT', 'Accountant','Docter','Engineer','Bussiness','Banker','Other'];
+  const occupationOptions = ['IT', 'Accountant', 'Doctor', 'Engineer', 'Business', 'Banker', 'Other'];
 
   const handleSelectChange = (e) => {
     const selectedOption = e.target.value;
@@ -65,6 +56,39 @@ const PartnerPreference = () => {
     }
   };
 
+  const handleOccupationSelect = (e) => {
+    const selectedOption = e.target.value;
+    if (selectedOption && !selectedOccupations.includes(selectedOption)) {
+      setSelectedOccupations([...selectedOccupations, selectedOption]);
+    }
+  };
+  const handleEducationSelect = (e) => {
+    const selectedOption = e.target.value;
+    if (selectedOption && !selectedEducations.includes(selectedOption)) {
+      setSelectedEducations([...selectedEducations, selectedOption]);
+    }
+  };
+  const handleReligionSelect = (e) => {
+    const selectedOption = e.target.value;
+    if (selectedOption && !selectedReligions.includes(selectedOption)) {
+      setSelectedReligions([...selectedReligions, selectedOption]);
+    }
+  };
+
+  const removeTag = (type, tag) => {
+    if (type === 'location') {
+      setSelectedLocations(selectedLocations.filter((location) => location !== tag));
+    } else if (type === 'hobby') {
+      setHobbies(hobbies.filter((hobby) => hobby !== tag));
+    } else if (type === 'occupation') {
+      setSelectedOccupations(selectedOccupations.filter((occupation) => occupation !== tag));
+    } else if (type === 'religion') {
+      setSelectedReligions(selectedReligions.filter((religion) => religion !== tag));
+    } else if (type === 'education') {
+      setSelectedEducations(selectedEducations.filter((education) => education !== tag));
+    }
+  };
+
   const handleSubmit = async () => {
     const preferences = {
       gender: selectedGender,
@@ -72,17 +96,15 @@ const PartnerPreference = () => {
       toAge: ageRange[1],
       district: selectedLocations,
       interest: hobbies,
-      religion: selectedReligion,
-      qualification: selectedEducation,
-      profession: selectedOccupation,
+      religion: selectedReligions,
+      qualification: selectedEducations,
+      profession: selectedOccupations,
       fromHeight: heightRange[0],
       toHeight: heightRange[1],
-
     };
 
     try {
       const response = await axiosPrivate.put(`/api/matrimony/profile/savePreferences/${matrimonyProfileId}`, preferences);
-
       if (response.status === 200) {
         toast.success("Preferences saved successfully");
         navigate('/locationSorting'); // Use navigate to programmatically navigate
@@ -98,36 +120,33 @@ const PartnerPreference = () => {
       console.error('Error:', error);
     }
   }
-useEffect(() => {
-  const getProfile = async () => {
-    try {
-      const { data } = await axiosPrivate.get(`/api/matrimony/profile/getProfile/${matrimonyProfileId}`);
-      if (data) {
-        setProfileData(data);
-        setProfilePic(data.profilePic || "assets/Images/defaultProfilePic.jpg");
 
-        // Ensure the fields are populated with either data from the profile or defaults
-        setAgeRange(data.preference ? [data.preference.fromAge, data.preference.toAge] : [18, 35]);
-        setHeightRange(data.preference ? [data.preference.fromHeight, data.preference.toHeight] : [100, 220]);
-        setHobbies(data.preference ? data.preference.interest || [] : []);
-        setSelectedGender(data.preference?.gender || '');
-        setSelectedLocations(data.preference?.district || []);
-        setSelectedEducation(data.preference?.qualification || '');
-        setSelectedReligion(data.preference?.religion || '');
-        setSelectedOccupation(data.preference?.profession || '');
-      } else {
-        console.error("Profile data is empty");
+  useEffect(() => {
+    const getProfile = async () => {
+      try {
+        const { data } = await axiosPrivate.get(`/api/matrimony/profile/getProfile/${matrimonyProfileId}`);
+        if (data) {
+          setProfileData(data);
+          setProfilePic(data.profilePic || "assets/Images/defaultProfilePic.jpg");
+          setAgeRange(data.preference ? [data.preference.fromAge, data.preference.toAge] : [18, 35]);
+          setHeightRange(data.preference ? [data.preference.fromHeight, data.preference.toHeight] : [100, 220]);
+          setHobbies(data.preference ? data.preference.interest || [] : []);
+          setSelectedGender(data.preference?.gender || '');
+          setSelectedLocations(data.preference?.district || []);
+          setSelectedEducations(data.preference?.qualification || []);
+          setSelectedReligions(data.preference?.religion || []);
+          setSelectedOccupations(data.preference?.profession || []);
+        } else {
+          console.error("Profile data is empty");
+        }
+      } catch (error) {
+        toast.error("Error fetching profile");
+        console.error('Error fetching profile:', error);
       }
-    } catch (error) {
-      toast.error("Error fetching profile");
-      console.error('Error fetching profile:', error);
-    }
-  };
+    };
 
-  getProfile();
-}, [matrimonyProfileId]);
-
-
+    getProfile();
+  }, [matrimonyProfileId]);
 
   return (
     <div className="activitycontainer">
@@ -139,7 +158,7 @@ useEffect(() => {
           <span className='search'><CiSearch /></span>
           <h1 className="title">Privacy & Settings</h1>
           <div className="profilePicContainer" onClick={toggleProfileOptions}>
-            <img src={profilePic || "assets/Images/defaultProfilePic.jpg"} alt="Profile" className="profilePic" /> {/* Fallback to default profile picture */}
+            <img src={profilePic || "assets/Images/defaultProfilePic.jpg"} alt="Profile" className="profilePic" />
           </div>
         </div>
 
@@ -155,6 +174,7 @@ useEffect(() => {
               text=""
             />
           </div>
+
           <div className="form-group">
             <label>Gender</label>
             <select
@@ -162,7 +182,7 @@ useEffect(() => {
               value={selectedGender}
               onChange={(e) => setSelectedGender(e.target.value)}
             >
-               <option value="">Value</option>
+              <option value="">Select </option>
               {genderOptions.map((gender) => (
                 <option key={gender} value={gender}>
                   {gender}
@@ -174,7 +194,7 @@ useEffect(() => {
           <div className="form-group">
             <label>Locations</label>
             <select className="form-control" onChange={handleSelectChange}>
-            <option value="">Value</option>
+              <option value="">Select</option>
               {keralaDistricts.map(district => (
                 <option key={district} value={district}>
                   {district}
@@ -189,10 +209,11 @@ useEffect(() => {
               ))}
             </div>
           </div>
+
           <div className="form-group">
             <label>Interests & Hobbies</label>
             <select className="form-control" onChange={handleInterestSelect}>
-              <option value="">Value</option>
+              <option value="">Select </option>
               {interestOptions.map(hobby => (
                 <option key={hobby} value={hobby}>
                   {hobby}
@@ -209,74 +230,80 @@ useEffect(() => {
           </div>
 
           <div className="form-group">
-            <label>Education Level</label>
-            <select
-              className="form-control"
-              value={selectedEducation}
-              onChange={(e) => setSelectedEducation(e.target.value)}  // Add onChange
-            >
-               <option value="">Value</option>
+            <label>Education</label>
+            <select className="form-control"  onChange={ handleEducationSelect}>
+              <option value="">Select</option> 
               {educationOptions.map((education) => (
                 <option key={education} value={education}>
                   {education}
                 </option>
               ))}
             </select>
-          </div>
-          <div className="form-group">
-            <RangeGroup
-              label="Height"
-              values={heightRange}
-              text="cm"
-              min={100}
-              max={220}
-              onChange={setHeightRange}
-            />
-
-          </div>
-          <div className="form-group">
-            <label>Religion</label>
-            <select
-              className="form-control"
-              value={selectedReligion}
-              onChange={(e) => setSelectedReligion(e.target.value)}  // Add onChange
-            >
-               <option value="">Value</option>
-              {religionOptions.map((religion) => (
-                <option key={religion} value={religion}>
-                  {religion}
-                </option>
+            <div className="tags">
+              {selectedEducations.map((education) => (
+                <span key={education}>
+                  {education} <button onClick={() => removeTag('education', education)}>x</button>
+                </span>
               ))}
-            </select>
+            </div>
           </div>
 
           <div className="form-group">
             <label>Occupation</label>
-            <select
-              className="form-control"
-              value={selectedOccupation}
-              onChange={(e) => setSelectedOccupation(e.target.value)}  // Add onChange
-            >
-               <option value="">Value</option>
+            <select className="form-control" onChange={handleOccupationSelect} >
+            <option value="">Select</option> 
               {occupationOptions.map((occupation) => (
                 <option key={occupation} value={occupation}>
                   {occupation}
                 </option>
               ))}
             </select>
+            <div className="tags">
+              {selectedOccupations.map((occupation) => (
+                <span key={occupation}>
+                  {occupation} <button onClick={() => removeTag('occupation', occupation)}>x</button>
+                </span>
+              ))}
+            </div>
           </div>
 
+          <FormGroup
+            label="Height"
+            values={heightRange}
+            min={100}
+            max={220}
+            onChange={setHeightRange}
+            text="cm"
+          />
 
-          {/* Submit Button */}
           <div className="form-group">
+            <label>Religion</label>
+            <select
+              className="form-control" onChange={handleReligionSelect} >
+                      <option value="">Select</option> 
+              {religionOptions.map((religion) => (
+                <option key={religion} value={religion}>
+                  {religion}
+                </option>
+              ))}
+            </select>
+            <div className="tags">
+              {selectedReligions.map((religion) => (
+                <span key={religion}>
+                  {religion} <button onClick={() => removeTag('religion', religion)}>x</button>
+                </span>
+              ))}
+            </div>
+          </div>
+
+           {/* Submit Button */}
+           <div className="form-group">
             <button className="submit-btn" onClick={handleSubmit}>
               Apply
             </button>
           </div>
         </div>
-      </div>
-
-      {
+      </div>      {
         showProfileOptions && (
           <div className="profileOptionsContainer">
             <BuddyHomeProfile toggleProfileOptions={toggleProfileOptions} />
@@ -284,15 +311,15 @@ useEffect(() => {
         )
       }
     </div >
-  );
-};
+)}
+
 
 const FormGroup = ({ label, text, values, min, max, onChange }) => (
   <div className="form-group">
     <div className="range-label">
       <label>{label}</label>
       <div className="range-values">
-        <span>{values[0]} - {values[1]}</span>
+        <span>{values[0]} - {values[1]} {text}</span> {/* Showing the current range */}
       </div>
     </div>
     <div className="range-slider-container">
@@ -305,21 +332,31 @@ const FormGroup = ({ label, text, values, min, max, onChange }) => (
           onChange={onChange}
           renderTrack={({ props, children }) => (
             <div
-              {...props}
+              onMouseDown={props.onMouseDown}
+              onTouchStart={props.onTouchStart}
               style={{
-                ...props.style,
-                height: "6px",
+                height: "36px",
+                display: "flex",
                 width: "100%",
-                background: getTrackBackground({
-                  values,
-                  colors: ["#4a90e2", "#ccc", "#4a90e2"],
-                  min,
-                  max
-                }),
-                borderRadius: "3px"
               }}
             >
-              {children}
+              <div
+                ref={props.ref}
+                style={{
+                  height: "5px",
+                  width: "100%",
+                  borderRadius: "4px",
+                  background: getTrackBackground({
+                    values,
+                    colors: ["#ccc", "#548BF4", "#ccc"],
+                    min,
+                    max
+                  }),
+                  alignSelf: "center"
+                }}
+              >
+                {children}
+              </div>
             </div>
           )}
           renderThumb={({ props }) => (
@@ -327,71 +364,14 @@ const FormGroup = ({ label, text, values, min, max, onChange }) => (
               {...props}
               style={{
                 ...props.style,
-                height: "20px",
-                width: "20px",
+                height: "24px",
+                width: "24px",
                 borderRadius: "50%",
-                backgroundColor: "#0a0a0a",
+                backgroundColor: "#FFF",
                 display: "flex",
                 justifyContent: "center",
                 alignItems: "center",
-                boxShadow: "0px 2px 6px #aaa"
-              }}
-            />
-          )}
-        />
-      </div>
-    </div>
-  </div>
-);
-
-const RangeGroup = ({ label, text, values, min, max, onChange }) => (
-  <div className="form-group">
-    <div className="range-label">
-      <label>{label}</label>
-      <div className="range-values">
-        <span>{values[0]} - {values[1]} {text}</span>
-      </div>
-    </div>
-    <div className="range-slider-container">
-      <div className="range-slider">
-        <Range
-          step={1}
-          min={min}
-          max={max}
-          values={values}
-          onChange={onChange}
-          renderTrack={({ props, children }) => (
-            <div
-              {...props}
-              style={{
-                ...props.style,
-                height: "6px",
-                width: "100%",
-                background: getTrackBackground({
-                  values,
-                  colors: ["#4a90e2", "#ccc", "#4a90e2"],
-                  min,
-                  max
-                }),
-                borderRadius: "3px"
-              }}
-            >
-              {children}
-            </div>
-          )}
-          renderThumb={({ props }) => (
-            <div
-              {...props}
-              style={{
-                ...props.style,
-                height: "20px",
-                width: "20px",
-                borderRadius: "50%",
-                backgroundColor: "#0a0a0a",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                boxShadow: "0px 2px 6px #aaa"
+                boxShadow: "0px 2px 6px #AAA"
               }}
             />
           )}

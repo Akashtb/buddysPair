35f3e34d60e3dba2fcde22fcec2ae4f02d93cb7b
anushmashaiju <anushma2015@gmail.com>
2024-09-23 19:51:31 +0530

    anushma17 arrayofchat time added block blur

--- frontend/src/components/ArrayOfChat/ArrayOfChat.jsx
+++ frontend/src/components/ArrayOfChat/ArrayOfChat.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
+import React from 'react';
+import './ArrayOfChat.css';
 
-function ArrayOfChat({message,own}) {
-    // console.log("each message",message);
-    
+function ArrayOfChat({ message, own }) {
+  const messageTime = new Date(message.createdAt).toLocaleTimeString([], {
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+
   return (
     <div>
-         <div key={message.id} className={`message ${own ?"sent":"received"}`}>
-                <div className="message-cont">
-                  {message.text}
-                </div>
-              </div>
+      <div key={message.id} className={`message ${own ? "sent" : "received"}`}>
+        <div className="message-cont">
+          {message.text}
+          <div className={`message-time ${own ? "own-time" : "received-time"}`}>{messageTime}</div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ArrayOfChat
+export default ArrayOfChat;

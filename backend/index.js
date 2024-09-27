81fe18851a3c7be983700a8ee2874daab50d4d72
Akash T B <akashtb21@gmail.com>
2024-09-27 16:22:49 +0530

    bulid is completed

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use(bodyParser.json());
 
 dotenv.config();
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: process.env.CORS,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
--- frontend/src/CustomApi/Axios.jsx
+++ frontend/src/CustomApi/Axios.jsx
@@ -1,12 +1,12 @@
 import axios from 'axios';
-const BASE_URL = 'http://localhost:8003';
+
 
 export default axios.create({
-    baseURL: BASE_URL
+    baseURL: import.meta.env.BASE_URL
 });
 
 export const axiosPrivate = axios.create({
-    baseURL: BASE_URL,
+    baseURL: import.meta.env.BASE_URL,
     headers: { 'Content-Type': 'application/json' },
     withCredentials: true
 });

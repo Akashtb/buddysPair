e443f9caef68483ec96df99a9fe35ec2c5eb0022
Akash T B <akashtb21@gmail.com>
2025-01-04 14:53:11 +0530

    project is completed

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use(bodyParser.json());
 
 dotenv.config();
 const corsOptions = {
-  origin: process.env.CORS,
+  origin: 'http://localhost:5173',
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
